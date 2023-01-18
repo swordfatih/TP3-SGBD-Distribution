5354c33a6d9620d6b9aad557b2763e801508ef02
swordfatih <accfldekur@gmail.com>
2023-01-18 16:27:28 +0100

    added script and modified grants and constraints

--- Script_Europe.sql
+++ Script_Europe.sql
@@ -1,3 +1,23 @@
+DROP DATABASE LINK L_Central;
+CREATE DATABASE LINK L_Central
+CONNECT TO fkilic IDENTIFIED BY "x"
+USING 'DB11';
+
+DROP DATABASE LINK L_Ameriques;
+CREATE DATABASE LINK L_Ameriques
+CONNECT TO fkilic IDENTIFIED BY "x"
+USING 'DB14';
+
+GRANT SELECT ON Fournisseurs TO mvieiraper;
+GRANT SELECT ON Clients_Europe_Du_Nord TO mvieiraper;
+GRANT SELECT ON Clients_Autres TO mvieiraper;
+GRANT SELECT ON Commandes_Europe_Du_Nord TO mvieiraper;
+GRANT SELECT ON Commandes_Autres TO mvieiraper;
+GRANT SELECT ON Details_Commandes_Europe_Du_Nord TO mvieiraper;
+GRANT SELECT ON Details_Commandes_Autres TO mvieiraper;
+GRANT SELECT ON Stock_Europe_Du_Nord TO mvieiraper;
+GRANT SELECT ON Stock_Autres TO mvieiraper;
+
 DROP TABLE Fournisseurs;
 CREATE TABLE Fournisseurs AS (
     SELECT *
@@ -55,3 +75,67 @@ CREATE TABLE Details_Commandes_Autres AS (
     SELECT NO_COMMANDE, REF_PRODUIT, PRIX_UNITAIRE, QUANTITE, REMISE
     FROM Ryori.Details_Commandes@L_Central NATURAL JOIN Commandes_Autres
 );
+
+CREATE SYNONYM Clients_Europe_Du_Sud 
+FOR mvieiraper.Clients_Europe_Du_Sud@L_Ameriques;
+
+CREATE SYNONYM Clients_Ameriques
+FOR mvieiraper.Clients_Ameriques@L_Ameriques;
+
+CREATE SYNONYM Commandes_Europe_Du_Sud
+FOR mvieiraper.Commandes_Europe_Du_Sud@L_Ameriques;
+
+CREATE SYNONYM Commandes_Ameriques
+FOR mvieiraper.Commandes_Ameriques@L_Ameriques;
+
+CREATE SYNONYM Details_Commandes_Europe_Du_Sud
+FOR mvieiraper.Details_Commandes_Europe_Du_Sud@L_Ameriques;
+
+CREATE SYNONYM Details_Commandes_Ameriques
+FOR mvieiraper.Details_Commandes_Ameriques@L_Ameriques;
+
+CREATE SYNONYM Stock_Europe_Du_Sud
+FOR mvieiraper.Stock_Europe_Du_Sud@L_Ameriques;
+
+CREATE SYNONYM Stock_Ameriques
+FOR mvieiraper.Stock_Ameriques@L_Ameriques;
+
+CREATE SYNONYM Employes
+FOR mvieiraper.Employes@L_Ameriques;
+
+CREATE OR REPLACE VIEW Clients AS
+SELECT * FROM Clients_Europe_Du_Nord
+UNION ALL
+SELECT * FROM Clients_Autres
+UNION ALL
+SELECT * FROM Clients_Europe_Du_Sud
+UNION ALL
+SELECT * FROM Clients_Ameriques;
+
+CREATE OR REPLACE VIEW Commandes AS
+SELECT * FROM Commandes_Europe_Du_Nord
+UNION ALL
+SELECT * FROM Commandes_Autres
+UNION ALL
+SELECT * FROM Commandes_Europe_Du_Sud
+UNION ALL
+SELECT * FROM Commandes_Ameriques;
+
+CREATE OR REPLACE VIEW Details_Commandes AS
+SELECT * FROM Details_Commandes_Europe_Du_Nord
+UNION ALL
+SELECT * FROM Details_Commandes_Autres
+UNION ALL
+SELECT * FROM Details_Commandes_Europe_Du_Sud
+UNION ALL
+SELECT * FROM Details_Commandes_Ameriques;
+
+CREATE OR REPLACE VIEW Stock AS
+SELECT * FROM Stock_Europe_Du_Nord
+UNION ALL
+SELECT * FROM Stock_Autres
+UNION ALL
+SELECT * FROM Stock_Europe_Du_Sud
+UNION ALL
+SELECT * FROM Stock_Ameriques;
+
