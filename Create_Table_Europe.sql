110d5046d4d3c0e1c531cebbed310c1cf1a2b447
swordfatih <accfldekur@gmail.com>
2023-01-18 14:36:21 +0100

    fix create tables for europe

--- Create_Table_Europe.sql
+++ Create_Table_Europe.sql
@@ -35,27 +35,25 @@ CREATE TABLE Clients_Autres AS (
 DROP TABLE Commandes_Europe_Du_Nord;
 CREATE TABLE Commandes_Europe_Du_Nord AS (
     SELECT CODE_CLIENT, SOCIETE, ADRESSE, VILLE, CODE_POSTAL, PAYS, TELEPHONE, FAX 
-    FROM Ryori.Commandes@L_DB11 NATURAL JOIN Ryori.Clients@L_DB11
-    WHERE Pays IN ('Norvege', 'Suede', 'Danemark', 'Islande', 'Finlande', 'Royaume-Uni', 'Irlande', 'Belgique', 'Luxembourg', 'Pays-Bas', 'Allemagne', 'Pologne')
+    FROM Ryori.Commandes@L_DB11 NATURAL JOIN Clients_Europe_Du_Nord
 );
 
 DROP TABLE Commandes_Autres;
 CREATE TABLE Commandes_Autres AS (
     SELECT CODE_CLIENT, SOCIETE, ADRESSE, VILLE, CODE_POSTAL, PAYS, TELEPHONE, FAX 
-    FROM Ryori.Commandes@L_DB11 NATURAL JOIN Ryori.Clients@L_DB11
-    WHERE PAYS NOT IN ('Norvège', 'Suède', 'Danemark', 'Islande', 'Finlande', 'Royaume-Uni', 'Irlande', 'Belgique', 'Luxembourg', 'Pays-Bas', 'Allemagne', 'Pologne', 'Antigua-et-Barbuda', 'Argentine', 'Bahamas', 'Barbade', 'Belize', 'Bolivie', 'Bresil', 'Canada', 'Chili', 'Colombie', 'Costa Rica', 'Cuba', 'Republique dominicaine', 'Dominique', 'Equateur', 'Etats-Unis', 'Grenade', 'Guatemala', 'Guyana', 'Haiti', 'Honduras', 'Jamaique', 'Mexique', 'Nicaragua', 'Panama', 'Paraguay', 'Perou', 'Saint-Christophe-et-Nieves', 'Sainte-Lucie', 'Saint-Vincent-et-les Grenadines', 'Salvador', 'Suriname', 'Trinite-et-Tobago', 'Uruguay', 'Venezuela', 'Espagne', 'Portugal', 'Andorre', 'France', 'Gibraltar', 'Italie', 'Saint-Marin', 'Vatican', 'Malte', 'Albanie', 'Bosnie-Herzegovine', 'Croatie', 'Grece', 'Macedoine', 'Montenegro', 'Serbie', 'Slovenie', 'Bulgarie')
+    FROM Ryori.Commandes@L_DB11 NATURAL JOIN Clients_Autres
 );
 
 DROP TABLE Details_Commandes_Europe_Du_Nord;
 CREATE TABLE Details_Commandes_Europe_Du_Nord AS (
     SELECT NO_COMMANDE, REF_PRODUIT, PRIX_UNITAIRE, QUANTITE, REMISE
-    FROM Commandes_Europe_Du_Nord NATURAL JOIN Ryori.Details_Commandes@L_DB11
+    FROM Ryori.Details_Commandes@L_DB11 NATURAL JOIN Commandes_Europe_Du_Nord
 );
 
 DROP TABLE Details_Commandes_Autres;
 CREATE TABLE Details_Commandes_Autres AS (
     SELECT NO_COMMANDE, REF_PRODUIT, PRIX_UNITAIRE, QUANTITE, REMISE
-    FROM Commandes_Autres NATURAL JOIN Ryori.Details_Commandes@L_DB11
+    FROM Ryori.Details_Commandes@L_DB11 NATURAL JOIN Commandes_Autres
 );
 
 GRANT SELECT, UPDATE, INSERT, DELETE ON Fournisseurs TO mvieiraper;
