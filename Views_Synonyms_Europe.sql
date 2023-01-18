3bcaa2dd77c43eac1d2e0675f24c4128468c85c2
swordfatih <accfldekur@gmail.com>
2023-01-18 16:51:56 +0100

    fix ameriques

--- Views_Synonyms_Europe.sql
+++ Views_Synonyms_Europe.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE SYNONYM Clients_Europe_Du_Sud 
 FOR mvieiraper.Clients_Europe_Du_Sud@L_Ameriques;
 
-CREATE SYNONYM Clients_Amerique
+CREATE SYNONYM Clients_Ameriques
 FOR mvieiraper.Clients_Amerique@L_Ameriques;
 
 CREATE SYNONYM Commandes_Europe_Du_Sud
 FOR mvieiraper.Commandes_Europe_Du_Sud@L_Ameriques;
 
-CREATE SYNONYM Commandes_Amerique
+CREATE SYNONYM Commandes_Ameriques
 FOR mvieiraper.Commandes_Amerique@L_Ameriques;
 
 CREATE SYNONYM Details_Commandes_Europe_Du_Sud
@@ -19,7 +19,7 @@ FOR mvieiraper.Details_Commandes_Ameriques@L_Ameriques;
 CREATE SYNONYM Stock_Europe_Du_Sud
 FOR mvieiraper.Stock_Europe_Du_Sud@L_Ameriques;
 
-CREATE SYNONYM Stock_Amerique
+CREATE SYNONYM Stock_Ameriques
 FOR mvieiraper.Stock_Amerique@L_Ameriques;
 
 CREATE SYNONYM Employes
@@ -32,7 +32,7 @@ SELECT * FROM Clients_Autres
 UNION ALL
 SELECT * FROM Clients_Europe_Du_Sud
 UNION ALL
-SELECT * FROM Clients_Amerique;
+SELECT * FROM Clients_Ameriques;
 
 CREATE OR REPLACE VIEW Commandes AS
 SELECT * FROM Commandes_Europe_Du_Nord
@@ -41,7 +41,7 @@ SELECT * FROM Commandes_Autres
 UNION ALL
 SELECT * FROM Commandes_Europe_Du_Sud
 UNION ALL
-SELECT * FROM Commandes_Amerique;
+SELECT * FROM Commandes_Ameriques;
 
 CREATE OR REPLACE VIEW Details_Commandes AS
 SELECT * FROM Details_Commandes_Europe_Du_Nord
@@ -50,7 +50,7 @@ SELECT * FROM Details_Commandes_Autres
 UNION ALL
 SELECT * FROM Details_Commandes_Europe_Du_Sud
 UNION ALL
-SELECT * FROM Details_Commandes_Amerique;
+SELECT * FROM Details_Commandes_Ameriques;
 
 CREATE OR REPLACE VIEW Stock AS
 SELECT * FROM Stock_Europe_Du_Nord
@@ -59,4 +59,4 @@ SELECT * FROM Stock_Autres
 UNION ALL
 SELECT * FROM Stock_Europe_Du_Sud
 UNION ALL
-SELECT * FROM Stock_Amerique;
+SELECT * FROM Stock_Ameriques;
