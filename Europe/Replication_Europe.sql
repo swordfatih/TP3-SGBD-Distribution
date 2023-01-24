e3a3c0dc77e3a0e71024430274be91d9d0183675
swordfatih <accfldekur@gmail.com>
2023-01-24 22:38:42 +0100

    added replication for Europe

--- Europe/Replication_Europe.sql
+++ Europe/Replication_Europe.sql
@@ -1,3 +1,24 @@
+-- Création de logs pour que l'amérique puisse faire un refresh fast
 CREATE MATERIALIZED VIEW LOG ON Fournisseurs;
 GRANT SELECT ON MLOG$_Fournisseurs TO mvieiraper;
 GRANT SELECT ON MLOG$_Fournisseurs TO avieiraper;
+
+-- Création des vues matérialisées
+DROP SYNONYM Employes;
+CREATE MATERIALIZED VIEW Employes
+REFRESH FAST
+NEXT SYSDATE + 1
+AS
+SELECT * FROM mvieiraper.Employes@L_Ameriques;
+
+DROP SYNONYM Categories;
+CREATE MATERIALIZED VIEW Categories
+REFRESH COMPLETE
+AS
+SELECT * FROM mvieiraper.Categories@L_Ameriques;
+
+DROP SYNONYM Produits;
+CREATE MATERIALIZED VIEW Produits
+REFRESH COMPLETE
+AS
+SELECT * FROM mvieiraper.Produits@L_Ameriques;
